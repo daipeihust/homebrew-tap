a1aa48160b51e66e4433b2e0e9b4adcb3eae263c
暮晨 <leisurelicht@gmail.com>
2023-05-01 00:35:01 +0800

    refactor: 💡 替换 url

--- im-select.rb
+++ im-select.rb
@@ -9,9 +9,9 @@ class ImSelect < Formula
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/daipeihust/im-select/raw/master/macOS/out/apple/im-select"
+    url "https://raw.githubusercontent.com/daipeihust/im-select/master/macOS/out/apple/im-select"
   else
-    url "https://github.com/daipeihust/im-select/raw/master/macOS/out/intel/im-select"
+    url "https://raw.githubusercontent.com/daipeihust/im-select/master/macOS/out/intel/im-select"
   end
 
 
